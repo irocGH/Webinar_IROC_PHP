3cafa956c7e62881ea8ae5b91d5f86dae5de915b
irocGH <famorba@gmail.com>
2022-02-16 21:52:47 -0600

    "indices"

---
+++ arreglos.php
@@ -0,0 +1,46 @@
+<?php
+class opAritmeticas{
+
+    function arreglo(){
+
+        $arreglo = array("Valor 1", "Valor 2", "Valor 3", "Valor 4");
+        //echo $arreglo[0];
+
+        $arreglo2 = array(1=>"pos 1", 2=>"pos 2", 0=>"pos x");
+        //echo $arreglo2[0];
+
+        $arreglo3[0]="pos cero";
+        $arreglo3[4]="pos cuatro";
+        //echo $arreglo3[4];
+    }
+    function suma($datosR){
+        $resultado = $datosR[0] + $datosR[1];
+        return $resultado;
+    }
+
+    function resta($uno, $dos, $tres, $cuatro, $cinco){
+        $resultado = $uno - $dos - $tres - $cuatro - $cinco;
+        return $resultado;
+    }
+
+    function multi($uno, $dos, $tres, $cuatro, $cinco){
+        $resultado = $uno * $dos * $tres * $cuatro *$cinco;
+        return $resultado;
+    }
+
+    function divide($uno, $dos, $tres, $cuatro, $cinco){
+        $resultado = $uno / $dos / $tres / $cuatro / $cinco;
+        return $resultado;
+    }
+
+    /*function mostrarValor(){
+        echo $resultado;
+    }*/
+}
+$datos = array(1,2,3,4);
+
+$objeto = new opAritmeticas();
+//$objeto->arreglo();
+
+echo "El valor es: ".$objeto->suma($datos);
+
